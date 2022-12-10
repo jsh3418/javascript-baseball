725c83801d22a5ec63d83b5a1a80343719928330
jsh3418 <albert5428@gmail.com>
2022-12-10 12:24:55 +0900

    feat: 숫자를 입력받는 기능 구현

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ class App {
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
     this.computerAnswer = this.makeComputerAnswer();
+    this.readAnswer();
+  }
+
+  readAnswer() {
+    Console.readLine("숫자를 입력해주세요 : ", (answer) => {
+      const playerAnswer = answer.split("");
+    });
   }
 
   makeComputerAnswer() {
