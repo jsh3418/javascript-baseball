998433a1a74fb14452be5bb3cc28b1ca435a406d
jsh3418 <albert5428@gmail.com>
2022-12-10 12:16:45 +0900

    feat: 랜덤 숫자로 정답 만듦 구현

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-const { Console } = require("@woowacourse/mission-utils");
+const { Console, Random } = require("@woowacourse/mission-utils");
 
 class App {
+  constructor() {
+    this.computerAnswer = [];
+  }
+
   play() {
     Console.print("숫자 야구 게임을 시작합니다.");
+    this.computerAnswer = this.makeComputerAnswer();
+  }
+
+  makeComputerAnswer() {
+    const computerAnswer = [];
+
+    while (computerAnswer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (!computerAnswer.includes(number)) {
+        computerAnswer.push(number);
+      }
+    }
+
+    return computerAnswer;
   }
 }
 
