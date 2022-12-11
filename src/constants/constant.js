282c3a7847b5a819649265bcf31d0f894e0f5edc
jsh3418 <albert5428@gmail.com>
2022-12-11 23:19:10 +0900

    feat: GAME 상수 작성

---
+++ src/constants/constant.js
@@ -0,0 +1,17 @@
+const GAME = {
+  MESSAGE: {
+    START: "숫자 야구 게임을 시작합니다.",
+    END: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+
+    REQUEST: {
+      GUESS_ANSWER: "숫자를 입력해주세요 : ",
+      RESTART_OR_NOT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n",
+    },
+
+    HINT: {
+      BALL: "볼",
+      STRIKE: "스트라이크",
+      NOTHING: "낫싱",
+    },
+  },
+};
