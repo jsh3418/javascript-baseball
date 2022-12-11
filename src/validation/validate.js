a4d834a1592a751f5170a06673881e3530cbdea3
jsh3418 <albert5428@gmail.com>
2022-12-11 11:53:00 +0900

    feat: 사용자가 중복된 숫자를 입력했을 때 에러처리

--- src/validation/validate.js
+++ src/validation/validate.js
@@ -3,6 +3,7 @@ const { GAME } = require("../constants/constant");
 const validateAnswer = (answer) => {
   validateAnswerIsNumber(answer);
   validateAnswerLength(answer);
+  validateDuplicateNumber(answer);
 };
 
 const validateAnswerIsNumber = (answer) => {
@@ -16,6 +17,12 @@ const validateAnswerIsNumber = (answer) => {
 const validateAnswerLength = (answer) => {
   if (answer.length !== GAME.ANSWER_LENGTH) {
     throw new Error(`[ERROR] 숫자 ${GAME.ANSWER_LENGTH}자리 입력해야 합니다.`);
+  }
+};
+
+const validateDuplicateNumber = (answer) => {
+  if (answer.length !== [...new Set(answer)].length) {
+    throw new Error(`${GAME.ANSWER_LENGTH}가지 숫자는 서로 다른 숫자여야 합니다.`);
   }
 };
 
